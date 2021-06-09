c68bbdb207ec134ce7556e70127b13002d3b2b4f
jfulop99 <66125323+jfulop99@users.noreply.github.com>
2021-06-09 17:02:44 +0200

    Movieservice

---
+++ movie/src/main/java/movie/MovieService.java
@@ -0,0 +1,15 @@
+package movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieService {
+
+    private List<Movie> movies;
+
+    public MovieService() {
+        movies = new ArrayList<>();
+    }
+
+
+}
