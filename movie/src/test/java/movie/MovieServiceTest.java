b57913f0795e3996b6336fada221736fff631fc5
jfulop99 <66125323+jfulop99@users.noreply.github.com>
2021-06-09 17:13:17 +0200

    Empty Tests

---
+++ movie/src/test/java/movie/MovieServiceTest.java
@@ -0,0 +1,25 @@
+package movie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getLatestMovie() {
+    }
+
+    @Test
+    void getMovieByName() {
+    }
+}
